7ba94bbc2daf5a58a1e052e81974ecb4d00a7a41
Yuffter <89680576+Yuffter@users.noreply.github.com>
2024-05-09 17:36:50 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-function clickButton() {
-    var btn = document.getElementById("btn_1");
-    btn.innerText = "clicked";
-}
-
 const apiURL = "https://script.google.com/macros/s/AKfycbzlISXKYbNZHT8c31dZUr3zqx1AGciFpD4QqTcHDlLhApN3zYCIJlzFq5QYSZwpFa9MSA/exec";
 const interval = 10;
+let myNumber = -1;
+
 function startSearch() {
+    if (document.getElementsByClassName("numberInput")[0].value == "") return;
+
+    myNumber = parseInt(document.getElementsByClassName("numberInput")[0].value);
+    saveData();
+
     let durations = {};
+
+    //APIからデータを取得
     fetch(apiURL)
     .then(function (fetch_data) {
-    return fetch_data.json();
+        return fetch_data.json();
     })
     .then(function (json) {
         for (var i in json) {
@@ -34,4 +38,23 @@ function startSearch() {
         outputText.innerText = 
         `${beforeHourAndMinute[0]}:${beforeHourAndMinute[1].toString().padStart(2,'0')} ~ ${afterHourAndMinute[0]}:${afterHourAndMinute[1].toString().padStart(2,'0')}`;
     });
+}
+
+function saveData() {
+    if (document.getElementsByClassName("numberInput")[0].value == "") return;
+
+    localStorage.setItem("myNumber",myNumber.toString());
+}
+
+function loadData() {
+    //初回入力の時
+    if (localStorage.getItem("myNumber") == null) return;
+
+    //一度保存していた場合
+    myNumber = parseInt(localStorage.getItem("myNumber"));
+}
+
+function onLoad() {
+    loadData();
+    document.getElementsByClassName("numberInput")[0].value = (myNumber == -1 ? "" : myNumber.toString());
 }
